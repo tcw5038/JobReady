341e3f246e9e81563164c4b3d58bff0ab2ac34a6
Tyler Warrick <tcw5038@gmail.com>
2019-02-10 11:53:43 -0800

    Trying to fix API URL

--- src/config.js
+++ src/config.js
@@ -1,6 +1,2 @@
-module.exports = {
-  PORT: process.env.PORT || 8080,
-  // other stuff
-  API_BASE_URL:
-    process.env.REACT_APP_API_BASE_URL || "http://localhost:3000/api"
-};
+exports.API_BASE_URL =
+  process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
