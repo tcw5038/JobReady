2432aa812bf3c4c391d896191a94ff9fb0c18934
Tyler Warrick <tcw5038@gmail.com>
2019-02-09 16:04:47 -0800

    Latest commit

--- src/config.js
+++ src/config.js
@@ -1,2 +1,6 @@
-export const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:8080/api";
+module.exports = {
+  PORT: process.env.PORT || 8080,
+  // other stuff
+  API_BASE_URL:
+    process.env.REACT_APP_API_BASE_URL || "http://localhost:3000/api"
+};
